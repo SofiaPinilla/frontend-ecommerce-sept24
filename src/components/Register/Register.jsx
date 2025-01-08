c876c059f8eef24d73b3e3403dede84d23376307
SofiaPinilla <contact@sofiapinilla.com>
2025-01-08 12:17:41 +0100

    first commit

---
+++ src/components/Register/Register.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import { register } from "../../features/auth/authSlice";
+import { useDispatch } from "react-redux";
+import { notification } from "antd";
+const Register = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+  const { name, email, password, password2 } = formData;
+  const dispatch = useDispatch();
+
+  const onChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (password !== password2) {
+      return notification.error({
+        message: "Error",
+        description: "Passwords do not match",
+      });
+    } else {
+      return dispatch(register(formData));
+    }
+  };
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        type="text"
+        name="name"
+        value={name}
+        onChange={onChange}
+        placeholder="Name"
+      />
+      <input
+        type="email"
+        name="email"
+        value={email}
+        onChange={onChange}
+        placeholder="Email"
+      />
+      <input
+        type="password"
+        name="password"
+        value={password}
+        onChange={onChange}
+        placeholder="Password"
+      />
+      <input
+        type="password"
+        name="password2"
+        value={password2}
+        onChange={onChange}
+        placeholder="Password 2"
+      />
+      <button type="submit">Register</button>
+    </form>
+  );
+};
+export default Register;
