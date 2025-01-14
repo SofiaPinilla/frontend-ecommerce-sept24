522b2c300e61a4a42ce96744c97a6fe3b28fd826
SofiaPinilla <contact@sofiapinilla.com>
2025-01-14 11:04:21 +0100

    guards & 404 added

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,49 @@
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Register from './components/Register/Register'
-import Login from './components/Login/Login'
-import Home from './components/Home/Home'
-import Header from './components/Header/Header'
-import Profile from './components/Profile/Profile'
-import PostDetail from './components/PostDetail/PostDetail'
-import Search from './components/Search/Search'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Register from "./components/Register/Register";
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
+import Profile from "./components/Profile/Profile";
+import PostDetail from "./components/PostDetail/PostDetail";
+import Search from "./components/Search/Search";
+import PrivateZone from "./guards/PrivateZone";
+import Admin from "./components/Admin/Admin";
+import AdminZone from "./guards/AdminZone";
+import NotFound from "./components/NotFound/NotFound";
 
 function App() {
-
   return (
     <>
-    <BrowserRouter>
-    <Header/>
-    <Routes>
-      <Route path="/register" element={ <Register/>} />
-      <Route path="/login" element={ <Login/>} />
-      <Route path="/" element={ <Home/>} />
-      <Route path="/profile" element={ <Profile/>} />
-      <Route path="/postDetail/:id" element={ <PostDetail/>} />
-      <Route path="/search/:title" element={ <Search/>} />
-    </Routes>
-    </BrowserRouter>
-    
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/admin"
+            element={
+              <AdminZone>
+                <Admin />
+              </AdminZone>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <PrivateZone>
+                <Profile />
+              </PrivateZone>
+            }
+          />
+          <Route path="/postDetail/:id" element={<PostDetail />} />
+          <Route path="/search/:title" element={<Search />} />
+          <Route path="*" element={<NotFound/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const API_URL = "http://localhost:3000/users";
+// const API_URL = "https://clase-mongoose-sept24.onrender.com/users";
 
 const register = async (user) => {
   const res = await axios.post(API_URL + "/create", user);
